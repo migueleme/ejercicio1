fadcfc49205fe00514d21b54dceac9bd69b04308
0017011 <0017011@PF01H52J.insags.com>
2020-09-17 10:30:41 +0200

    javaDoc

--- autoriego/src/main/java/com/viewnext/autoriego/business/service/CultivoService.java
+++ autoriego/src/main/java/com/viewnext/autoriego/business/service/CultivoService.java
@@ -5,7 +5,11 @@ import java.util.Optional;
 
 import com.viewnext.autoriego.persistence.model.Cultivo;
 
-
+/**
+ * Servicio que se encarga de gestionar los cultivos
+ * @author 0017011
+ *
+ */
 public interface CultivoService {
 
 	public List<Cultivo> getAll();
